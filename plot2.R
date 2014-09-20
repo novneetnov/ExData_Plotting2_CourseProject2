2b1fc3065ea51686901b850e1c7d351e392c8fa4
novneetnov <novneet346iith@gmail.com>
2014-09-20 21:08:49 +0530

    plot2.R and plot2.png

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 ## Subset the dataset NEI containing only the data for the Baltimore City.
-NEI_Baltimore = subset(NEI, fips=="24510", select=c(Emissions,year))
+NEI_Baltimore = subset(NEI, fips=="24510")
 
 ## Split the Emisions column by year and sum over all observations of the 
 ## baltimore cityof a particular year to get the total emission
